849537d238a2be728ce1658ede088fbd79125501
wwr2005 <wwr20005@gmail.com>
2023-09-10 18:28:32 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача №1");
+        for (int a = 1; a <= 10; a++){
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        System.out.println("Задача №2");
+
+    }
+}
